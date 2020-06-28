4f43a94eb5c772730177db96f46cdb2c751559a9
Klaybson <49767964+Klaybson@users.noreply.github.com>
2020-06-27 22:35:26 -0300

    Update backup2.sh

--- backup2.sh
+++ backup2.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 # Variaveis
-DATA='date + %d-%m-%y-%H.%M'Impressões
+DADOS = ' data +% d-% m-% y-% H.% M ' Impressões
 uname -isv
 
 
-echo -n "O que deseja fazer? (A)Acessar Bk /(B)Backup do /var/www / (C)BK do MySQL (S)air? [A] "
-read resposta
-case "$resposta" in
-    a|A|"")
-        if [ -d "/var/backup" ] 
+echo -n " O que você deseja fazer? (A) Acessar Bk / (B)ackup pagina web (C) BK no MySQL (S) Sair?"
+head resposta
+caso  " $ resposta "  in
+    a | A | " " )
+        if [ -d  " / var / backup " ]
 		then
-			echo "Diretorio não existia"
+			echo  " Diretorio não existe"
+			cd / var / backup
+		else
+			echo "Diretorio criado /var/backup"
 			mkdir /var/backup
 			cd /var/backup
 		fi
     ;;
-    b|B)
-       tar -zcvf www-$DATA.tar.gz /var/www  
+    b | B)
+       tar -zcvf www- $ DATA .tar.gz / var / www  
     ;;
-    c|C)
+    c | C)
        /etc/init.d/mysql stop 
-		tar -zcvf mysql-$DATA.tar.gz /var/lib/mysql
+		tar -zcvf mysql- $ DATA .tar.gz / var / lib / mysql
 		/etc/init.d/mysql start 
 	;;	
-	s|S)
-		echo "saindo"	
+	s | S)
+		echo  " saindo "	
 	;;
-    *)
-        echo "Opção inválida"
+    * )
+        echo  " Opção inválida "
     ;;
 esac
