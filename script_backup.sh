62b74fbee0dc8038d433e836f843dc8cce75cdcc
Klaybson <49767964+Klaybson@users.noreply.github.com>
2020-06-28 15:08:00 -0300

    Update script_backup.sh

--- script_backup.sh
+++ script_backup.sh
@@ -1,66 +1,70 @@
 #!/bin/bash
-echo "==============================Olá===================================="
+clear
 uname -isv
+echo ''
+echo "======================================Ola==============================================="
+echo ""
 hostname
-echo "===========O que você deseja fazer?===============
+echo ""
+echo "============================== Escolha o servico =======================================
 (a)Diretorio de Backup.
 (b)Backup da pagina Web.
-(c)Backup do MySQL.
+(c)Backup do MySQL (stop e restart).
 (s)Sair?."
 read resposta
-while [ "$resposta" != "s" ]
-do
-        case  "$resposta"  in
-            a | A | " " )
-                if [ ! -d "/var/backups" ]; then
-                mkdir /var/backups
-                cd /var/backups
-                echo "Diretorio criado"
-        else
 
-                echo  " Diretorio já existe"
-                cd /var/backups
-        fi
-           ;;
-           b | B)
-                #compacta com data e hora
-                tar -cvzf  www-$(date +%d-%m-%Y_%H-%M-%S).tar.gz /var/www
-        #Verifica se o diretorio de backup existe. Se existir move
-       if [ ! -d "/var/backups" ]; then
-                echo "Diretorio */var/backups* inexistente "
-                echo "Arquivo Salvo no diretorio:"
-                pwd
-       else
-                mv *.gz /var/backups
-        fi
-
-       ;;
-       c | C)
-               /etc/init.d/mysql stop
-               tar -cvzf mysql-$(date +%d-%m-%Y_%H-%M-%S).tar.gz /var/lib/mysql
-               clear
-               /etc/init.d/mysql start
-        #Verifica se o diretorio de backup existe. Se existir move       
-        if [ ! -d "/var/backups" ]; then
-                echo "Diretorio */var/backups* inexistente "
-                echo "Arquivo salvo no diretorio:"
-                pwd
-       else
-                mv *.gz /var/backups
-        fi
-    ;;
-    s | S)
-                echo  " Saindo... "
-      ;;
-      * )
-        echo  " Opção inválida!"
-    ;;
-
-esac
-echo "===========O que você deseja fazer?===============
-(a)Diretorio de Backup.
-(b)Backup da pagina Web.
-(c)Backup do MySQL.
-(s)Sair?."
-read resposta
-done
+	while [ "$resposta" != "s" ]
+	do
+		case  "$resposta"  in
+		
+		a | A | " " )
+			if [ ! -d "/var/backups" ]; then
+				mkdir /var/backups
+				cd /var/backups
+				echo "Diretorio criado"
+			else
+				echo  " Diretorio já existe"
+				cd /var/backups
+			fi
+		;;
+		b | B)
+			#compacta com data e hora
+			tar -cvzf  www-$(date +%d-%m-%Y_%H-%M-%S).tar.gz /var/www
+			#Verifica se o diretorio de backup existe. Se existir move
+		    if [ ! -d "/var/backups" ]; then
+					echo "Diretorio */var/backups* inexistente "
+					echo "Arquivo Salvo no diretorio:"
+					pwd
+		    else
+					mv *.gz /var/backups
+			fi
+		;;
+		c | C)
+			
+			/etc/init.d/mysql stop
+			tar -cvzf mysql-$(date +%d-%m-%Y_%H-%M-%S).tar.gz /var/lib/mysql
+			clear
+			/etc/init.d/mysql start
+			#Verifica se o diretorio de backup existe. Se existir move       
+			if [ ! -d "/var/backups" ]; then
+				echo "Diretorio */var/backups* inexistente "
+				echo "Arquivo salvo no diretorio:"
+				pwd
+ 		    else
+				mv *.gz /var/backups
+			fi
+		;;
+		s | S)
+			echo  " Saindo... "
+		;;
+		* )
+			echo  " Opcao invalida!"
+		;;
+		esac
+	echo "===========O que você deseja fazer?===============
+	(a)Diretorio de Backup.
+	(b)Backup da pagina Web.
+	(c)Backup do MySQL.
+	(s)Sair?."
+	read resposta
+done 
